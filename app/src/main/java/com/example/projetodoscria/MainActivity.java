bf276fdc727dff023b0f2c7086b0c28ea1d3a4fb
Fridilipi <felippesouza97@outlook.com>
2018-01-10 01:41:45 -0200

    AGORA VAI

--- app/src/main/java/com/example/projetodoscria/MainActivity.java
+++ app/src/main/java/com/example/projetodoscria/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "ACERTÔ", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "XAMPSON", Toast.LENGTH_LONG).show();
     }
 }
