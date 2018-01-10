f425fdbac2d20eaf5ed2702350d4dc86da9ea2c9
Bian97 <bianmedeiros@hotmail.com>
2018-01-10 01:40:04 -0200

    batata

--- app/src/main/java/com/example/projetodoscria/MainActivity.java
+++ app/src/main/java/com/example/projetodoscria/MainActivity.java
@@ -11,10 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "SOU FODA", Toast.LENGTH_LONG).show();
-
-        Toast.makeText(MainActivity.this, "SOU FODA, NA CAMA EU TE ESCULACHO!", Toast.LENGTH_LONG).show();
-
-        Toast.makeText(MainActivity.this, "XAMPSON", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "ACERTÔ", Toast.LENGTH_LONG).show();
     }
 }
